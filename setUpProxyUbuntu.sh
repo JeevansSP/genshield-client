27ddf0054634016b0d72c74400866147bbb540e8
JeevansSP <jeevansp07@gmail.com>
2024-06-09 01:07:07 +0530

    req: add

---
+++ setUpProxyUbuntu.sh
@@ -0,0 +1,5 @@
+gsettings set org.gnome.system.proxy mode 'manual' 
+gsettings set org.gnome.system.proxy.http host 'localhost'
+gsettings set org.gnome.system.proxy.http port 8080
+gsettings set org.gnome.system.proxy.https host 'localhost'
+gsettings set org.gnome.system.proxy.https port 8080
