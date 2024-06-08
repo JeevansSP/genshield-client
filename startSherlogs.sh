7d27bf42413920a0947d226c2f6374c149947679
JeevansSP <jeevansp07@gmail.com>
2024-06-09 05:26:11 +0530

    changes

--- startSherlogs.sh
+++ startSherlogs.sh
@@ -2,7 +2,7 @@
 export http_proxy="http://localhost:8080" 
 export https_proxy="http://localhost:8080"
 
-mitmdump -p 8080 -s 
+mitmdump -p 8080 -s "./interceptor.py" 
 
                                                                                                                                        
                                                                                                                                        
