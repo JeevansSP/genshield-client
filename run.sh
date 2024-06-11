e76faa6d22e8335053addd15e17dbbd8e402e83f
JeevansSP <jeevansp07@gmail.com>
2024-06-12 00:09:47 +0530

    change: run.sh

--- run.sh
+++ run.sh
@@ -16,12 +16,17 @@ echo "SHERLOGS_API_KEY set to $SHERLOGS_API_KEY"
 
 # Set up system-wide proxy settings for HTTP and HTTPS
 echo "Setting up system-wide HTTP and HTTPS proxy settings..."
+
 export http_proxy="http://localhost:8080"
 export https_proxy="http://localhost:8080"
+export HTTP_PROXY="http://localhost:8080"
+export HTTPS_PROXY="http://localhost:8080"
 
 # Append proxy settings to .profile to ensure they persist after reboot
 echo "export http_proxy=\"http://localhost:8080\"" >> ~/.profile
 echo "export https_proxy=\"http://localhost:8080\"" >> ~/.profile
+echo "export HTTP_PROXY=\"http://localhost:8080\"" >> ~/.profile
+echo "export HTTPS_PROXY=\"http://localhost:8080\"" >> ~/.profile
 
 # For sudo commands to keep proxy settings
 echo "Defaults env_keep += \"http_proxy https_proxy\"" | sudo tee -a /etc/sudoers
@@ -33,9 +38,9 @@ echo "HTTPS Proxy: $https_proxy"
 
 # Start mitmdump in the background and redirect its output to a log file
 echo "Starting mitmdump to handle HTTP and HTTPS traffic..."
-mitmdump -p 8080 -s ./interceptor.py > mitmdump.log  2>&1 
+mitmdump -p 8080 -s ./interceptor.py 
 
-MITMDUMP_PID=$!
-echo "mitmdump is running with PID $MITMDUMP_PID, logging to mitmdump.log."
+# MITMDUMP_PID=$!
+# echo "mitmdump is running with PID $MITMDUMP_PID, logging to mitmdump.log."
 
-echo "Setup is complete and system proxy settings are applied."
+# echo "Setup is complete and system proxy settings are applied."
